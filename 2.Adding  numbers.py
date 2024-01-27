9660bf391f88b2db567a8fc50b359264e11da68d
Yuvraj Singh <77912320+yuvraj-cyber@users.noreply.github.com>
2024-01-27 10:25:56 +0530

    Add files via upload

---
+++ 2.Adding  numbers.py
@@ -0,0 +1,27 @@
+#1
+m = input("First Number: ")
+n = input("Second Number: ")
+
+print(m + n , '\n')
+#remember by default python will interpret any input as a string
+
+#2
+x = int(input("First Integer: "))
+y = int(input("Second Integer: "))
+
+'''#here we are using "int" which represents Integer. Hence we will
+only get Integers as input and output#'''
+
+print(x + y, '\n')
+#Here {'\n'} gives us a new line after output. This has nothing to do with the code.
+
+
+#3
+
+a = float(input("First Float: "))
+b = float(input("Second Float: "))
+
+'''here we are using "float" which represents Decimal. Hence we will
+only get Float as input and output'''
+
+print(a + b, '\n')
