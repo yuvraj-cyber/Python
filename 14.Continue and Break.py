a4f2d7b472e161616c9aa9cb18c052a86ae74b8a
Yuvraj Singh <77912320+yuvraj-cyber@users.noreply.github.com>
2024-02-08 20:22:55 +0530

    Add files via upload

---
+++ 14.Continue and Break.py
@@ -0,0 +1,10 @@
+while True:
+    n = int(input("Enter the number you want to calculate root: "))
+    if n < 0:
+        continue
+
+    else:
+        break
+
+for i in range(1):
+    print(n**(1/2))
